8d03b5ccabb44330c555b6a3d31a1f96ad0070b0
Mitchell Rodríguez <mitrodle@gmail.com>
2017-03-29 14:54:28 -0500

    bienvenida

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("registrar").addEventListener("click", function(){
+
+	window.location = "registrar.html"
+
+});
+
+document.getElementById("iniciar").addEventListener("click", function(){
+
+	window.location = "inicio.html"
+	
+});
